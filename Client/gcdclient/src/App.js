1d53ff1b5201321df687a1915c076f99092a9822
Joseph Warwick <JWarwick@sgfleet.com>
2021-10-22 10:53:04 +1100

    more deployment fix attempts

--- Client/gcdclient/src/App.js
+++ Client/gcdclient/src/App.js
@@ -4,6 +4,7 @@ import Loading from './components/Loading'
 import { useEffect, useState } from 'react'
 import ToDoService from './service/ToDoService'
 import AddForm from './components/AddForm'
+import Footer from './components/Footer'
 
 function App() {
   const [todos, setTodos] = useState(null)
@@ -23,7 +24,7 @@ function App() {
     newTodo.checked = !newTodo.checked
     newTodos[todoItemIndex] = newTodo
     setTodos(newTodos)
-    ToDoService.updateTodo(newTodo)
+    ToDoService.updateTodo(newTodo).catch(msg => console.log(msg));
   }
 
   function onAddTodo(title){
@@ -32,7 +33,7 @@ function App() {
     const newTodo = { todoItemId:0, title: title, checked: false, scheduledy: new Date(), added: new Date() }
     newTodos.push(newTodo)
     setTodos(newTodos)
-    ToDoService.addTodo(newTodo)
+    ToDoService.addTodo(newTodo).catch(msg => console.log(msg));
   }
 
   function onDeleteTodo(todo){
@@ -40,17 +41,19 @@ function App() {
     const newTodos = [...todos]
     newTodos.splice(todoItemIndex, 1);
     setTodos(newTodos)
-    ToDoService.delTodo(todo.todoItemId);
+    ToDoService.delTodo(todo.todoItemId).catch(msg => console.log(msg));
   }
 
   return (
     <div className="App">
+      <h2>ToDo List</h2>
       <AddForm onAddTodo={onAddTodo} />
       {todos ? (
         <MainContent todos={todos}  onUpdateTodo={onUpdateTodo} onDeleteTodo={onDeleteTodo} /> 
       ) : (
        <Loading />
       )}
+      <Footer />
     </div>
   )
 }
